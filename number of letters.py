a2e1c0409ad761d442b4be212f23a401b3b7a863
MarcelIsrafilov <89697275+MarcelIsrafilov@users.noreply.github.com>
2021-09-04 13:57:11 +0200

    adding first program
    It counts the amount of each letter in a sentence you give.

---
+++ number of letters.py
@@ -0,0 +1,14 @@
+sentence = input("Give a sentence to count the letters in: ")   #ask for a sentence
+list_zin = []
+for i in sentence:
+    list_zin.append(i)                                          #make a list of the sentence and make it all lowercase
+
+number_of_letters = []                                          #add a list to count all the characters
+
+for i in range(97, 123):
+    number_of_letters.append((list_zin.count(chr(i)), chr(i)))  #count all the characters a-z
+
+number_of_letters.sort(reverse=True)                            #sort the list in descending order of amount of letters
+print("This is the number of each letter in the sentence: ")
+for i in number_of_letters:
+    print(i[1]+ ": " + str(i[0]))
