37c2c69282a94f67477ac013370e289cfe7e64fc
MarcelIsrafilov <israfilov.marcel@gmail.com>
2021-09-05 21:07:55 +0200

    commit on 'adding-file-reading-function'

--- number of letters.py
+++ number of letters.py
@@ -1,21 +1,26 @@
-sentence = input("Give a sentence to count the letters in: ")           #ask for a sentence
-list_sentence = []
-for i in sentence:
-    list_sentence.append(i.lower())                                     #make a list of the sentence and make it all lowercase
+try:
+    text = open(input("Give a file with its directory to count the lettetrs in"))   #ask for a text
+except FileNotFoundError:
+    print("This file wasn't found, please check for any mistakes in the name")      #if the file doesn't exist it gives an error
 
-number_of_letters = []                                                  #add a list to count all the characters
+readtext = text.read()                                                              
+list_text = []
+for i in readtext:
+    list_text.append(i.lower())                                                     #make a list of the text and make it all lowercase
+
+number_of_letters = []                                                              #add a list to count all the characters
 
 for i in range(97, 123):
-    number_of_letters.append((list_sentence.count(chr(i)), chr(i)))     #count all the characters a-z
+    number_of_letters.append((list_text.count(chr(i)), chr(i)))                     #count all the characters a-z
 
-number_of_letters.sort(reverse=True)                                    #sort the list in descending order of amount of letters
+number_of_letters.sort(reverse=True)                                                #sort the list in descending order of amount of letters
 print("This is the number of each letter in the sentence: ")
-all_occur = True                                                        #adding a variable which checks whether all letters of the alphabet were found
-for i in number_of_letters:                                             #printing the number of letters
+all_occur = True                                                                    #adding a variable which checks whether all letters of the alphabet were found
+for i in number_of_letters:                                                         #printing the number of letters
     if i[0]>0:
         print(i[1]+ ": " + str(i[0]))
     else:
         all_occur = False
 
-if not all_occur:                                                       #says whether or not all letters occured
+if not all_occur:                                                                   #says whether or not all letters occured
     print("The rest of the letter(s) didn't occur in the sentence")
