1d145038438f0296a32dca2245dafad40ff80045
MarcelIsrafilov <israfilov.marcel@gmail.com>
2021-09-12 17:36:44 +0200

    commit on 'adding-file-reading-function'

--- number of letters.py
+++ number of letters.py
@@ -1,14 +1,14 @@
 try:
-    text = open(input("Give a file with its directory to count the lettetrs in"))   #ask for a text
+    text = open(input("Give a file with its directory to count the lettetrs in: "))
 except FileNotFoundError:
-    print("This file wasn't found, please check for any mistakes in the name")      #if the file doesn't exist it gives an error
+    print("This file wasn't found, please check for any mistakes in the name or directory")    
 
 readtext = text.read()                                                              
 list_text = []
 for i in readtext:
     list_text.append(i.lower())                                                     #make a list of the text and make it all lowercase
 
-number_of_letters = []                                                              #add a list to count all the characters
+number_of_letters = []                                                              
 
 for i in range(97, 123):
     number_of_letters.append((list_text.count(chr(i)), chr(i)))                     #count all the characters a-z
@@ -16,7 +16,7 @@ for i in range(97, 123):
 number_of_letters.sort(reverse=True)                                                #sort the list in descending order of amount of letters
 print("This is the number of each letter in the sentence: ")
 all_occur = True                                                                    #adding a variable which checks whether all letters of the alphabet were found
-for i in number_of_letters:                                                         #printing the number of letters
+for i in number_of_letters:                                                         
     if i[0]>0:
         print(i[1]+ ": " + str(i[0]))
     else:
